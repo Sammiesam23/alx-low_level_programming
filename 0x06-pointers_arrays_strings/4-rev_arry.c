a07ca1b35542f41511d734239bdda5d7f73c9047
Samuel Ohihon <ohihons@gmail.com>
2023-03-03 02:47:23 -0800

    4-rev_arry.c

---
+++ 0x06-pointers_arrays_strings/4-rev_arry.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * write a function that reverses
+ * the content of an array of integers.
+ * @a: The first parameter
+ * @n: The second parameter
+ */
+
+void reverse_array(int *a, int n)
+{
+	int rev, arr;
+
+	for (arr = n - 1; arr >= n / 2; arr--)
+	{
+		rev = a[n - 1 - arr];
+		a[n - 1 - arr] = a[arr];
+		a[arr] = rev;
+	}
+}
