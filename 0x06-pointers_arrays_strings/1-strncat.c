e3e66b2dd427a9d84c4bee200363dccbd0586bc2
Samuel Ohihon <ohihons@gmail.com>
2023-03-02 16:12:20 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,27 @@
-include <stdio.h>
-int main() 
+#include <stdio.h>
+#int main() 
 {
 char s1[100] = "programming ", s2[] = "is awesome";
 int length, j;
 }
-// store length of s1 in the length variable;
+//store length of s1 in the length variable;
 length = 0;
-while (s1[length] != '\0');
+while (s1[length] != '\0')
 {
 ++length;
 }
 
-// concatenate s2 to s1
+//concatenate s2 to s1
 for (j = 0; s2[j] != '\0';
-++j, ++length) 
+++j, ++length); 
 {
 s1[length] = s2[j];
 }
 
-// terminating the s1 string
+/ terminating the s1 string
 {
 s1[length] = '\0';
 printf("After concatenation: ");
 puts(s1);
-return 0;
+return 0
 }
